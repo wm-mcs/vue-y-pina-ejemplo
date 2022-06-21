140d30e7fd20818345577aea75ab83f0b1610db4
Mauricio <ventas@worldmaster.com.uy>
2022-06-21 10:35:12 -0300

    ok

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,29 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { onBeforeMount, onMounted, ref, reactive, computed } from "vue";
+import Layout from "./components/layoute.vue";
+
+const text = ref("MAURICO");
+const data = ref([{ name: "Pablo" }, { name: "Mario" }]);
+
+function buttonClick() {
+  data.value.push({ name: text.value });
+  text.value = "Defecto";
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <Layout msg="Mensaje" foo="De Mauricio">
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <h2>{{ text }}</h2>
+    <input type="text" v-model="text" />
+
+    <button @click="buttonClick">Agregar</button>
+
+    <div v-for="item in data">
+      {{ item.name }}
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </Layout>
 </template>
 
-<style>
-@import './assets/base.css';
-
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-}
-</style>
+<style></style>
---
+++ src/components/layoute.vue
@@ -0,0 +1,25 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+  foo: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="w-full">
+    <div>
+      Header <b>{{ msg }} {{ foo }}</b>
+    </div>
+    <slot></slot>
+
+    <div>Footer</div>
+  </div>
+</template>
+
+<style scoped></style>
